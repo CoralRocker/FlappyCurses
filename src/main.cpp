a22cd2c82ad07965b1b35943ff50acecd19e85a5
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-04-11 08:10:49 -0500

    Menu Change

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int main()
 
 	/* Menu */
 	menu startMenu(stdscr);
-	startMenu.setOpts(3, "Easy", "Medium", "Quit");
+	startMenu.setOpts(4, "Easy", "Medium", "Hard", "Quit");
 MENURUN:
 	char choice = startMenu.runMenu();
 	switch(choice)
@@ -59,6 +59,9 @@ MENURUN:
 			speed = 50;
 			break;
 		case 2:
+			speed = 25;
+			break;
+		case 3:
 			endwin();
 			return -1;
 		default:
@@ -112,7 +115,7 @@ MENURUN:
 				{
 					score++;
 					pipes[0].passed = true;
-					speed -= 2;
+					speed -= 4;
 				}
 				else if(!pipes[0].birdIn(player))
 				{
